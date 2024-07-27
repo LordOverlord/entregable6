8b9334a57c4ea17431bbb42aaf0dce1ad4bdc95d
Rafael Camacho <overlordco@gmail.com>
2024-07-27 03:23:27 -0600

    first commit

---
+++ entregable6/script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login form submitted!');
+});
+
+document.getElementById('contactForm')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Contact form submitted!');
+});
